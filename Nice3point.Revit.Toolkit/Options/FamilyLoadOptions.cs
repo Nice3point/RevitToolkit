63f7e810f7be66e6d8b60c574fcbaa1ba6e577a7
Nice3point <nice3point@gmail.com>
2022-07-22 12:45:48 +0300

    new FamilyLoadOptions

---
+++ Nice3point.Revit.Toolkit/Options/FamilyLoadOptions.cs
@@ -0,0 +1,42 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace Nice3point.Revit.Toolkit.Options;
+
+public class FamilyLoadOptions : IFamilyLoadOptions
+{
+    public bool OnFamilyFound(bool familyInUse, out bool overwriteParameterValues)
+    {
+        overwriteParameterValues = false;
+        if (familyInUse)
+        {
+            var taskDialog = new TaskDialog("Family already exists");
+            taskDialog.TitleAutoPrefix = false;
+            taskDialog.MainInstruction = "You are trying to load the family which already exists in this project. What do you want to do?";
+            taskDialog.CommonButtons = TaskDialogCommonButtons.Cancel;
+            taskDialog.DefaultButton = TaskDialogResult.Cancel;
+            taskDialog.AddCommandLink(TaskDialogCommandLinkId.CommandLink1, "Overwrite the existing version");
+            taskDialog.AddCommandLink(TaskDialogCommandLinkId.CommandLink2, "Overwrite the existing version and its parameter values");
+            var result = taskDialog.Show();
+            switch (result)
+            {
+                case TaskDialogResult.Cancel:
+                    return false;
+                case TaskDialogResult.CommandLink2:
+                    overwriteParameterValues = false;
+                    break;
+                default:
+                    throw new NotSupportedException();
+            }
+        }
+
+        return true;
+    }
+
+    public bool OnSharedFamilyFound(Family sharedFamily, bool familyInUse, out FamilySource source, out bool overwriteParameterValues)
+    {
+        source = FamilySource.Family;
+        overwriteParameterValues = false;
+        return true;
+    }
+}

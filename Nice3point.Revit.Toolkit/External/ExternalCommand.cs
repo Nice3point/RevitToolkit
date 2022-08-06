79860058baf72e7a9ea02ff89ee319b75cea1995
Nice3point <nice3point@gmail.com>
2022-08-06 23:02:05 +0300

    CleanUp

--- Nice3point.Revit.Toolkit/External/ExternalCommand.cs
+++ Nice3point.Revit.Toolkit/External/ExternalCommand.cs
@@ -5,7 +5,7 @@ using Autodesk.Revit.UI;
 namespace Nice3point.Revit.Toolkit.External;
 
 /// <summary>
-///    Implementation for a Revit add-in External Command
+///     Implementation for a Revit add-in External Command
 /// </summary>
 public abstract class ExternalCommand : IExternalCommand
 {
@@ -15,22 +15,22 @@ public abstract class ExternalCommand : IExternalCommand
     public ElementSet ElementSet { get; private set; }
 
     /// <summary>
-    ///     Reference to the <see cref="Autodesk.Revit.UI.ExternalCommandData"/> that is needed by an external command
+    ///     Reference to the <see cref="Autodesk.Revit.UI.ExternalCommandData" /> that is needed by an external command
     /// </summary>
     public ExternalCommandData ExternalCommandData { get; private set; }
 
     /// <summary>
-    ///     Reference to the <see cref="Autodesk.Revit.UI.UIApplication"/> that is needed by an external command
+    ///     Reference to the <see cref="Autodesk.Revit.UI.UIApplication" /> that is needed by an external command
     /// </summary>
     public UIApplication UiApplication { get; set; }
 
     /// <summary>
-    ///     Reference to the <see cref="Autodesk.Revit.UI.UIDocument"/> that is needed by an external command
+    ///     Reference to the <see cref="Autodesk.Revit.UI.UIDocument" /> that is needed by an external command
     /// </summary>
     public UIDocument UiDocument => UiApplication.ActiveUIDocument;
 
     /// <summary>
-    ///     Reference to the <see cref="Autodesk.Revit.DB.Document"/> that is needed by an external command
+    ///     Reference to the <see cref="Autodesk.Revit.DB.Document" /> that is needed by an external command
     /// </summary>
     public Document Document => UiApplication.ActiveUIDocument.Document;
 
@@ -45,12 +45,12 @@ public abstract class ExternalCommand : IExternalCommand
     /// </summary>
     /// <remarks>
     ///     The result indicates if the execution fails, succeeds, or was canceled by user. If it does not
-    /// succeed, Revit will undo any changes made by the external command
+    ///     succeed, Revit will undo any changes made by the external command
     /// </remarks>
     public Result Result { get; set; } = Result.Succeeded;
 
     /// <summary>
-    ///     Error message can be returned by external command. This will be displayed only if the command status was "Failed" <br/>
+    ///     Error message can be returned by external command. This will be displayed only if the command status was "Failed" <br />
     ///     There is a limit of 1023 characters for this message; strings longer than this will be truncated.
     /// </summary>
     public string ErrorMessage { get; set; } = string.Empty;

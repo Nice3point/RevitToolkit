589b7656659816e052e369231dc4deef762fdd3d
Nice3point <nice3point@gmail.com>
2022-11-19 01:32:45 +0300

    New AsyncEventHandler

--- Nice3point.Revit.Toolkit/External/Handlers/ActionEventHandler.cs
+++ Nice3point.Revit.Toolkit/External/Handlers/ActionEventHandler.cs
@@ -1,10 +1,12 @@
-﻿using Autodesk.Revit.UI;
+﻿using System.ComponentModel;
+using Autodesk.Revit.UI;
 
 namespace Nice3point.Revit.Toolkit.External.Handlers;
 
 /// <summary>
-///     Handler, to provide access to change the Revit document from modeless windows
+///     Handler, to provide access to change the Revit document with the ability to queue calls to Raise methods 
 /// </summary>
+[PublicAPI]
 public class ActionEventHandler : ExternalEventHandler
 {
     private Action<UIApplication> _action;
@@ -12,13 +14,23 @@ public class ActionEventHandler : ExternalEventHandler
     /// <summary>
     ///     This method is called to handle the external event
     /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
     public override void Execute(UIApplication uiApplication)
     {
-        _action?.Invoke(uiApplication);
+        if (_action is null) return;
+
+        try
+        {
+            _action(uiApplication);
+        }
+        finally
+        {
+            _action = null;
+        }
     }
 
     /// <summary>
-    ///     Instructing Revit to raise (signal) the external event
+    ///     Instructing Revit to queue a handler and raise (signal) the external event
     /// </summary>
     /// <remarks>
     ///     Revit will wait until it is ready to process the event and then
@@ -27,137 +39,20 @@ public class ActionEventHandler : ExternalEventHandler
     ///     edit modes are currently active in Revit, which is the same policy
     ///     like the one that applies to evoking external commands
     /// </remarks>
-    /// <returns>
-    ///     The result of event raising request. If the request is 'Accepted',
-    ///     the event would be added to the event queue and its handler will
-    ///     be executed in the next event-processing cycle
-    /// </returns>
-    public void Raise(Action<UIApplication> action)
+    public void Raise([NotNull] Action<UIApplication> action)
     {
-        _action = action;
+        if (_action is null) _action = action;
+        else _action += action;
+
         Raise();
     }
-}
-
-/// <summary>
-///     Handler, to provide access to change the Revit document from modeless windows
-/// </summary>
-public class ActionEventHandler<T> : ExternalEventHandler
-{
-    private Action<UIApplication, T> _action;
-    private T _param;
 
     /// <summary>
-    ///     This method is called to handle the external event
+    ///     Clears the call queue of subscribed delegates
     /// </summary>
-    public override void Execute(UIApplication uiApplication)
+    /// <remarks>The queue can be cleaned up before the first delegate is invoked</remarks>
+    public void Cancel()
     {
-        _action?.Invoke(uiApplication, _param);
-    }
-
-    /// <summary>
-    ///     Instructing Revit to raise (signal) the external event
-    /// </summary>
-    /// <remarks>
-    ///     Revit will wait until it is ready to process the event and then
-    ///     it will execute its event handler by calling the Execute method.
-    ///     Revit processes external events only when no other commands or
-    ///     edit modes are currently active in Revit, which is the same policy
-    ///     like the one that applies to evoking external commands
-    /// </remarks>
-    /// <returns>
-    ///     The result of event raising request. If the request is 'Accepted',
-    ///     the event would be added to the event queue and its handler will
-    ///     be executed in the next event-processing cycle
-    /// </returns>
-    public void Raise(T param, Action<UIApplication, T> action)
-    {
-        _param = param;
-        _action = action;
-        Raise();
-    }
-}
-
-/// <summary>
-///     Handler, to provide access to change the Revit document from modeless windows
-/// </summary>
-public class ActionEventHandler<T0, T1> : ExternalEventHandler
-{
-    private Action<UIApplication, T0, T1> _action;
-    private T0 _param0;
-    private T1 _param1;
-
-    /// <summary>
-    ///     This method is called to handle the external event
-    /// </summary>
-    public override void Execute(UIApplication uiApplication)
-    {
-        _action?.Invoke(uiApplication, _param0, _param1);
-    }
-
-    /// <summary>
-    ///     Instructing Revit to raise (signal) the external event
-    /// </summary>
-    /// <remarks>
-    ///     Revit will wait until it is ready to process the event and then
-    ///     it will execute its event handler by calling the Execute method.
-    ///     Revit processes external events only when no other commands or
-    ///     edit modes are currently active in Revit, which is the same policy
-    ///     like the one that applies to evoking external commands
-    /// </remarks>
-    /// <returns>
-    ///     The result of event raising request. If the request is 'Accepted',
-    ///     the event would be added to the event queue and its handler will
-    ///     be executed in the next event-processing cycle
-    /// </returns>
-    public void Raise(T0 param0, T1 param1, Action<UIApplication, T0, T1> action)
-    {
-        _param0 = param0;
-        _param1 = param1;
-        _action = action;
-        Raise();
-    }
-}
-
-/// <summary>
-///     Handler, to provide access to change the Revit document from modeless windows
-/// </summary>
-public class ActionEventHandler<T0, T1, T2> : ExternalEventHandler
-{
-    private Action<UIApplication, T0, T1, T2> _action;
-    private T0 _param0;
-    private T1 _param1;
-    private T2 _param2;
-
-    /// <summary>
-    ///     This method is called to handle the external event
-    /// </summary>
-    public override void Execute(UIApplication uiApplication)
-    {
-        _action?.Invoke(uiApplication, _param0, _param1, _param2);
-    }
-
-    /// <summary>
-    ///     Instructing Revit to raise (signal) the external event
-    /// </summary>
-    /// <remarks>
-    ///     Revit will wait until it is ready to process the event and then
-    ///     it will execute its event handler by calling the Execute method.
-    ///     Revit processes external events only when no other commands or
-    ///     edit modes are currently active in Revit, which is the same policy
-    ///     like the one that applies to evoking external commands
-    /// </remarks>
-    /// <returns>
-    ///     The result of event raising request. If the request is 'Accepted',
-    ///     the event would be added to the event queue and its handler will
-    ///     be executed in the next event-processing cycle
-    /// </returns>
-    public void Raise(T0 param0, T1 param1, T2 param2, Action<UIApplication, T0, T1, T2> action)
-    {
-        _param0 = param0;
-        _param1 = param1;
-        _param2 = param2;
-        _action = action;
-        Raise();
+        _action = null;
     }
 }
---
+++ Nice3point.Revit.Toolkit/External/Handlers/AsyncEventHandler.cs
@@ -0,0 +1,117 @@
+﻿using System.ComponentModel;
+using Autodesk.Revit.UI;
+
+namespace Nice3point.Revit.Toolkit.External.Handlers;
+
+/// <summary>
+///     Handler, to provide access to change the Revit document
+/// </summary>
+/// <remarks>Suitable for cases where it is needed to await the completion of an external event</remarks>
+[PublicAPI]
+public sealed class AsyncEventHandler : ExternalEventHandler
+{
+    private Action<UIApplication> _action;
+    private TaskCompletionSource<bool> _resultTask;
+
+    /// <summary>
+    ///     This method is called to handle the external event
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public override void Execute(UIApplication uiApplication)
+    {
+        if (_action is null)
+        {
+            _resultTask.SetResult(false);
+            return;
+        }
+
+        try
+        {
+            _action(uiApplication);
+            _resultTask.SetResult(false);
+        }
+        catch (Exception exception)
+        {
+            _resultTask.SetException(exception);
+        }
+        finally
+        {
+            _action = null;
+            _resultTask = null;
+        }
+    }
+
+    /// <summary>
+    ///     Instructing Revit to queue a handler, raise (signal) the external event and async awaiting for its completion
+    /// </summary>
+    /// <remarks>
+    ///     This method async awaiting completion of the <see cref="Nice3point.Revit.Toolkit.External.Handlers.AsyncEventHandler.Execute"/> method. <br/>
+    ///     Exceptions in the delegate will not be ignored and will be rethrown in the original synchronization context.<br/>
+    ///     Use <see langword="await" /> to awaiting. <see cref="Task.WaitAll(System.Threading.Tasks.Task[])"/>, <see cref="Task.Wait()"/> will cause a deadlock.
+    /// </remarks>
+    public async Task RaiseAsync([NotNull] Action<UIApplication> action)
+    {
+        if (_action is null) _action = action;
+        else _action += action;
+
+        Raise();
+
+        _resultTask ??= new TaskCompletionSource<bool>();
+        await _resultTask.Task;
+    }
+}
+
+/// <summary>
+///     Handler, to provide access to change the Revit document
+/// </summary>
+/// <remarks>Suitable for cases where it is needed to await the completion of an external event with the return of a value</remarks>
+[PublicAPI]
+public sealed class AsyncEventHandler<T> : ExternalEventHandler
+{
+    private Func<UIApplication, T> _func;
+    private TaskCompletionSource<T> _resultTask;
+
+    /// <summary>
+    ///     This method is called to handle the external event
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public override void Execute(UIApplication uiApplication)
+    {
+        try
+        {
+            var result = _func(uiApplication);
+            _resultTask.SetResult(result);
+        }
+        catch (Exception exception)
+        {
+            _resultTask.SetException(exception);
+        }
+        finally
+        {
+            _func = null;
+            _resultTask = null;
+        }
+    }
+
+    /// <summary>
+    ///     Instructing Revit to queue a handler, raise (signal) the external event and async awaiting for its completion
+    /// </summary>
+    /// <returns>
+    ///     The return value of the method that delegate encapsulates
+    /// </returns>
+    /// <remarks>
+    ///     This method async awaiting completion of the <see cref="Nice3point.Revit.Toolkit.External.Handlers.AsyncEventHandler.Execute"/> method. <br/>
+    ///     Exceptions in the delegate will not be ignored and will be rethrown in the original synchronization context.<br/>
+    ///     Use <see langword="await" /> to awaiting. <see cref="Task.WaitAll(System.Threading.Tasks.Task[])"/>, <see cref="Task.Wait()"/> will cause a deadlock.
+    /// </remarks>
+    public async Task<T> RaiseAsync([NotNull] Func<UIApplication, T> func)
+    {
+        if (_func is null) _func = func;
+        else _func += func;
+
+        Raise();
+
+        _resultTask ??= new TaskCompletionSource<T>();
+        return await _resultTask.Task;
+    }
+}

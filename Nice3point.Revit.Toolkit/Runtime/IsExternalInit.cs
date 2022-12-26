c3e528c9b9a9b7dcd656093a2a78ab408f740700
Nice3point <nice3point@gmail.com>
2022-12-26 19:16:33 +0300

    Runtime attributes

---
+++ Nice3point.Revit.Toolkit/Runtime/IsExternalInit.cs
@@ -0,0 +1,22 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.ComponentModel;
+
+// ReSharper disable once CheckNamespace
+namespace System.Runtime.CompilerServices;
+
+/// <summary>
+///     Reserved to be used by the compiler for tracking metadata.
+///     This class should not be used by developers in source code.
+///     This dummy class is required to compile records when targeting .NET Standard
+/// </summary>
+[EditorBrowsable(EditorBrowsableState.Never)]
+#if BUILDING_IOT_DEVICE_BINDINGS
+internal
+#else
+public
+#endif
+    static class IsExternalInit
+{
+}
---
+++ Nice3point.Revit.Toolkit/Runtime/RequiredMemberAttribute.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// ReSharper disable once CheckNamespace
+
+namespace System.Runtime.CompilerServices;
+
+/// <summary>Specifies that a type has required members or that a member is required.</summary>
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Field | AttributeTargets.Property, Inherited = false)]
+#if SYSTEM_PRIVATE_CORELIB
+public
+#else
+internal
+#endif
+    sealed class RequiredMemberAttribute : Attribute
+{
+}

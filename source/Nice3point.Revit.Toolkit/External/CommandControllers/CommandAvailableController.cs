1a80061cb2f15790e855bc6d93ccbf696c8cfd4c
Nice3point <nice3point@gmail.com>
2024-06-22 14:52:53 +0300

    Add AvailableCommandController

---
+++ source/Nice3point.Revit.Toolkit/External/CommandControllers/CommandAvailableController.cs
@@ -0,0 +1,18 @@
+﻿using Autodesk.Revit.UI;
+
+namespace Nice3point.Revit.Toolkit.External.CommandControllers;
+
+/// <summary>
+///     Controller providing permanent accessibility for External Command invocation
+/// </summary>
+[PublicAPI]
+public sealed class AvailableCommandController : IExternalCommandAvailability
+{
+    /// <summary>
+    ///     The command is always available for execution on the ribbon
+    /// </summary>
+    public bool IsCommandAvailable(UIApplication applicationData, CategorySet selectedCategories)
+    {
+        return true;
+    }
+}

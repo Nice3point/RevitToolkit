16537bc11dc5d23c2574fbcd204c537e45986010
Nice3point <nice3point@gmail.com>
2025-01-17 20:57:19 +0300

    Update workflow

--- build/Build.Configuration.cs
+++ build/Build.Configuration.cs
@@ -5,16 +5,12 @@ sealed partial class Build
     readonly AbsolutePath ArtifactsDirectory = RootDirectory / "output";
     readonly AbsolutePath ChangeLogPath = RootDirectory / "Changelog.md";
 
-    string ReleaseVersion => ((GitRepository.Branch is null && GitRepository.Tags.Count > 0) ||
-                              (GitRepository.Branch is not null && GitRepository.Branch.StartsWith("refs/tags"))) switch
-    {
-        true when GitHubActions.Instance is not null => GitHubActions.Instance.RefName,
-        true => GitRepository.Tags.Single(),
-        false => "1.0.0"
-    };
-    
+    [Parameter] string ReleaseVersion;
+
     protected override void OnBuildInitialized()
     {
+        ReleaseVersion = GitRepository.Tags.SingleOrDefault();
+
         Configurations =
         [
             "Release*"

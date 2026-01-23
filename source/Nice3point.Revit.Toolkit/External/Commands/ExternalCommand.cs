3012c4b750dfe9374839c0ab7c7ca4fb064183e2
Nice3point <nice3point@gmail.com>
2026-01-23 21:28:11 +0300

    [Cleanup] Move built-in properties to the beginning

--- source/Nice3point.Revit.Toolkit/External/Commands/ExternalCommand.cs
+++ source/Nice3point.Revit.Toolkit/External/Commands/ExternalCommand.cs
@@ -17,14 +17,29 @@ namespace Nice3point.Revit.Toolkit.External;
 public abstract class ExternalCommand : IExternalCommand
 {
     /// <summary>
+    ///     Reference to the <see cref="Autodesk.Revit.UI.ExternalCommandData" /> that is needed by an external command
+    /// </summary>
+    public ExternalCommandData ExternalCommandData { get; private set; } = null!;
+
+    /// <summary>
+    ///     Error message can be returned by external command. This will be displayed only if the command status was "Failed" <br />
+    ///     There is a limit of 1023 characters for this message; strings longer than this will be truncated.
+    /// </summary>
+    public string ErrorMessage { get; set; } = string.Empty;
+
+    /// <summary>
     ///     Element set indicating problem elements to display in the failure dialog. This will be used only if the command status was "Failed".
     /// </summary>
     public ElementSet ElementSet { get; private set; } = null!;
 
     /// <summary>
-    ///     Reference to the <see cref="Autodesk.Revit.UI.ExternalCommandData" /> that is needed by an external command
+    ///     Informs Autodesk Revit of the status of your application after execution.
     /// </summary>
-    public ExternalCommandData ExternalCommandData { get; private set; } = null!;
+    /// <remarks>
+    ///     The result indicates if the execution fails, succeeds, or was canceled by user. If it does not
+    ///     succeed, Revit will undo any changes made by the external command
+    /// </remarks>
+    public Result Result { get; set; } = Result.Succeeded;
 
     /// <summary>
     ///     Represents an active session of the Autodesk Revit user interface, providing access to
@@ -43,7 +58,7 @@ public abstract class ExternalCommand : IExternalCommand
     /// </remarks>
     /// <exception cref="T:Autodesk.Revit.Exceptions.InvalidOperationException">Thrown when attempting to modify the property.</exception>
     public UIDocument ActiveUiDocument => RevitContext.ActiveUiDocument!;
-    
+
     /// <summary></summary>
     [Obsolete("Use ActiveUiDocument instead")]
     [CodeTemplate(
@@ -71,21 +86,6 @@ public abstract class ExternalCommand : IExternalCommand
 
     /// <summary>Represents the currently active view.</summary>
     public View ActiveView => RevitContext.ActiveView!;
-
-    /// <summary>
-    ///     Informs Autodesk Revit of the status of your application after execution.
-    /// </summary>
-    /// <remarks>
-    ///     The result indicates if the execution fails, succeeds, or was canceled by user. If it does not
-    ///     succeed, Revit will undo any changes made by the external command
-    /// </remarks>
-    public Result Result { get; set; } = Result.Succeeded;
-
-    /// <summary>
-    ///     Error message can be returned by external command. This will be displayed only if the command status was "Failed" <br />
-    ///     There is a limit of 1023 characters for this message; strings longer than this will be truncated.
-    /// </summary>
-    public string ErrorMessage { get; set; } = string.Empty;
 
     /// <summary>Callback invoked by Revit. Not used to be called in user code</summary>
     [EditorBrowsable(EditorBrowsableState.Never)]
